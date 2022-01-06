9176f93b5e4eee07e848029231ceeee99ca63836
Anna <acdodd17@gmail.com>
2022-01-05 20:09:04 -0500

    undo last commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
-const { viewDepartments, addDepartment } = require('./src/departments');
-const { viewRoles, addRole} = require('./src/roles');
-const { viewEmployees, addEmployee, updateEmployee} = require('./src/employees');
+const { printTable } = require('console-table-printer');
 
 // connect to database
 db.connect(err => {
@@ -39,9 +37,175 @@ const start = () => {
     });
 };
 
+// view all departments
+const viewDepartments = () => {
+    const sql = `SELECT * FROM departments`
+    db.query(sql, (err, rows) => {
+        if (err) {
+            throw err; 
+        }
+        printTable(rows);
+        start();
+    });
+};
 
+// view all roles
+const viewRoles = () => {
+    const sql = `SELECT roles.id, roles.title, roles.salary, departments.name AS department
+                FROM roles
+                INNER JOIN departments ON roles.department_id = departments.id`
+    db.query(sql, (err, rows) => {
+        if (err) {
+            throw err; 
+        }
+        printTable(rows);
+        start();
+    });
+};
 
+// view all employees
+const viewEmployees = () => {
+    const sql = `SELECT e.id, e.first_name, e.last_name,
+                roles.title AS role,
+                departments.name AS department,
+                roles.salary AS salary,
+                CONCAT(m.first_name, ' ', m.last_name) AS manager
+                FROM employees e
+                INNER JOIN roles ON e.role_id = roles.id
+                LEFT JOIN departments on e.role_id = roles.id AND roles.department_id = departments.id
+                LEFT JOIN employees m ON m.id = e.manager_id
+    `
+    db.query(sql, (err, rows) => {
+        if (err) {
+            throw err; 
+        }
+        printTable(rows);
+        start();
+    });
+};
 
+// add a department
+const addDepartment = () => {
+    inquirer. prompt(
+        {
+            type: 'input',
+            name: 'newDepartment', 
+            message: 'What is the name of the department you would like to add?'
+        }
+    ).then(response => {
+        const sql = `INSERT INTO departments (name) VALUES (?)`
+        const params = response.newDepartment;
+
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                throw err; 
+            }
+            console.log('New department added!')
+            viewDepartments();
+        });
+    });
+};
+
+// add a role
+const addRole = () => {
+    db.query('SELECT * FROM departments', (err, rows) => {
+        if (err) throw err; 
+        const departmentList = rows.map (department => {
+            return {
+                name: department.name, 
+                value: department.id
+            }
+        })
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title', 
+                message: 'What is the name of the new role you would like to add?'
+            }, 
+            {
+                type: 'input', 
+                name: 'salary', 
+                message: 'What is the salary for this new role?'
+            },
+            {
+                type: 'list',
+                name: 'department_id', 
+                message: 'What is the id of the department to which this role will be added?', 
+                choices: departmentList
+            }])
+            .then(response => {
+                const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`
+                const params = [
+                    response.title, 
+                    response.salary, 
+                    response.department_id
+                ];
+                db.query(sql, params, (err, result) => {
+                    if (err) {
+                        throw err; 
+                    }
+                    console.log('New role successfully added!')
+                    viewRoles();
+                });
+            });
+    });
+};
+
+// add an employee
+const addEmployee = () => {
+    // query to get all roles
+    // query to get employees with manager_id null
+    // inquirer prompt new employee first_name, last_name, role from all roles, and manager from list of all managers
+    // query to INSERT into employees : first_name, last_name, role_id, manager_id
+};
+
+// update an employee role
+const updateEmployee = () => {
+    db.query('SELECT * FROM roles', (err, rows) => {
+        if (err) throw err; 
+        const rolesList = rows.map( role => {
+            return {
+                name:role.title, 
+                value: role.id
+            }
+        })
+        db.query('SELECT * FROM employees', (err, rows) => {
+            if (err) throw err;
+            const employeeList = rows.map (employee => {
+                return {
+                    name: employee.first_name + ' ' + employee.last_name, 
+                    value: employee.id
+                }
+            })
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'employee', 
+                    message:'What is the name of the employee you would like to update?', 
+                    choices: employeeList
+                }, 
+                {
+                    type: 'list', 
+                    name: 'newTitle', 
+                    message: 'What new title will this employee hold?',
+                    choices: rolesList
+                }
+            ])
+            .then(response => {
+                const sql = `UPDATE employees SET role_id = ? WHERE id = ?`
+                const params = [
+                    response.employee, 
+                    response.newTitle
+                ]
+                db.query(sql, params, (err, result) => {
+                    if (err) throw err;
+                    console.log ('Employee role updated!')
+                })
+                viewEmployees();
+            })
+        }); 
+    });
+};
 
 
 
