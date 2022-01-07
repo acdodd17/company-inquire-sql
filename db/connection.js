c8e31356ee9aec1a47b6242d978e7a6e6d64fca1
Anna <acdodd17@gmail.com>
2022-01-07 08:24:47 -0500

    password protect

--- db/connection.js
+++ db/connection.js
@@ -1,48 +1,14 @@
 //import mysql
 const mysql = require('mysql2');
 
-//import secure password 
-var securePassword = require('secure-password');
-
-// Initialise our password policy
-var pwd = securePassword()
- 
-var userPassword = Buffer.from('MySQLacdr@tt3rr331713!')
- 
-// Register user
-pwd.hash(userPassword, function (err, hash) {
-  if (err) throw err
- 
-  // Save hash somewhere
-  pwd.verify(userPassword, hash, function (err, result) {
-    if (err) throw err
-    
-    switch (result) {
-      case securePassword.INVALID_UNRECOGNIZED_HASH:
-        return console.error('This hash was not made with secure-password. Attempt legacy algorithm')
-      case securePassword.INVALID:
-        return console.log('Invalid password')
-      case securePassword.VALID:
-        return console.log('Authenticated')
-      case securePassword.VALID_NEEDS_REHASH:
-        console.log('Yay you made it, wait for us to improve your safety')
- 
-        pwd.hash(userPassword, function (err, improvedHash) {
-          if (err) console.error('You are authenticated, but we could not improve your safety this time around')
- 
-          // Save improvedHash somewhere
-        })
-        break
-    }
-  });
-});
+require('dotenv').config();
 
 //connect to database
 const db = mysql.createConnection(
     {
         host: 'localHost', 
         user: 'root', 
-        password: userPassword, 
+        password: process.env.DB_PW, 
         database: 'company'
     }, 
     console.log('Connected to the company database')
